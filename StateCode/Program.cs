a014550462e96f23e559ad413c324af0acac07f4
Parth Patel <parth2002715@gmail.com>
2021-09-30 10:58:59 -0400

    Add project files.

---
+++ StateCode/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StateCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var states = new List<State>()
+            {
+                new State("OH", "Ohio"),
+                new State("IN", "Indiana"),
+                new State("KY", "Kentucky")
+            };
+                //var stateoh = new State("OH", "Ohio");
+                //states.Add(stateoh);
+                //var stateky = new State("KY", "Kentucky");
+                //states.Add(stateky);
+                
+                //states.Add(new State("IN", "Indiana"));
+
+            foreach (var STE in states)
+            {
+                Console.WriteLine($"The State code {STE.Code} is representing {STE.Name}");
+            }
+
+
+
+
+
+            //var statecodes = new Dictionary<string, string>()
+            //{ 
+            //    ["OH"] = "Ohio", ["KY"] = "Kentucky", ["IN"] = "Indiana"
+            //};
+            //statecodes.Add("OH", "Ohio");
+            //statecodes.Add("IN", "Indiana");
+            //statecodes.Add("KY", "Kentucky");
+
+            //var ohio = statecodes["OH"];
+            //Console.WriteLine(ohio);
+        }
+    }
+}
---
+++ StateCode/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateCode
+{
+    class State
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public State(string code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
+
+    }
+}
